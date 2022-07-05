4d4223e62a5e1591bbb368fa3e5a1381563d0e93
dai2424 <2506171542@qq.com>
2022-07-05 18:45:03 +0800

    init

---
+++ emsystem-api/src/main/java/com/jit/emsystemapi/EmsystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.jit.emsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsystemApiApplication.class, args);
+    }
+
+}
